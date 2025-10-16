2b4421e543758a6273dca8b35b9b8876f9f9ec1b
nhquang2509 <nguyenhuuquangmss@gmail.com>
2025-10-16 15:42:27 +0700

    lab2-p1

---
+++ Assets/Scirpts/InteractableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string ItemName;
+
+    public string GetItemName()
+    {
+        return ItemName;
+    }
+}
---
+++ Assets/Scirpts/SelectionManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    public GameObject interaction_Info_UI;
+    TMP_Text interaction_text;
+
+    private void Start()
+    {
+        interaction_text = interaction_Info_UI.GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selectionTransform = hit.transform;
+
+            if (selectionTransform.GetComponent<InteractableObject>())
+            {
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
+            }
+            else
+            {
+                interaction_Info_UI.SetActive(false);
+            }
+
+        }
+    }
+}
